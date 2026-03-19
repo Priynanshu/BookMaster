cc677a36782dd2d264ec3261c3380bb5600a7cc2
Priyanshu Kumar <priyanshu3960@gmail.com>
2026-03-19 20:53:25 +0530

    adding vercel origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
    origin: [
-    "http://localhost:5173",
+    "https://book-master-ruddy.vercel.app",
     "chrome-extension://*" // ← extension ke liye
   ],
     credentials: true
