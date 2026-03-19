fb696c47b036c05e49aec955792d649ef44b7642
Priyanshu Kumar <priyanshu3960@gmail.com>
2026-03-19 22:22:39 +0530

    Fix CORS for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,6 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
    origin: [
-    "http://localhost:5173",
     "https://your-frontend.vercel.app", // ← Vercel URL
     "chrome-extension://*",
   ],
